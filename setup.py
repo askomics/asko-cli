abeccf614cef37efff0b0754417eccb0b40b8e68
xgaia <xgaia@gmx.com>
2017-03-01 18:04:30 +0100

    update setup.py with download_url

--- setup.py
+++ setup.py
@@ -3,22 +3,23 @@ from setuptools import setup, find_packages
 from os import listdir
 
 setup(
-    name="askocli",
+    name='askocli',
     version='0.2',
-    description="Command line interface for a distant AskOmics",
-    author="Xavier Garnier",
-    author_email="xavier.garnier@irisa.fr",
-    url="https://github.com/xgaia/asko-cli",
+    description='Command line interface for a distant AskOmics',
+    author='Xavier Garnier',
+    author_email='xavier.garnier@irisa.fr',
+    url='https://github.com/xgaia/asko-cli',
+    download_url='https://github.com/askomics/asko-cli/archive/v0.1.tar.gz',
     install_requires=['requests>=2.4.3'],
     packages=find_packages(),
     license='AGPL',
-    platforms="Posix; MacOS X; Windows",
+    platforms='Posix; MacOS X; Windows',
     scripts=['bin/' + f for f in listdir('bin')],
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: AGPL License",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering",
-        "Programming Language :: Python :: 3"
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: AGPL License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering',
+        'Programming Language :: Python :: 3'
     ])
