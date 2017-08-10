7f261074e08cc133008aeb85c55ec14b7e8224cb
xgaia <xgaia@gmx.com>
2017-08-10 15:48:57 +0200

    travis auto deploy on pypi

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='askocli',
-    version='0.4.1',
+    version='0.4.2',
     description='Command line interface for a distant AskOmics',
     author='Xavier Garnier',
     author_email='xavier.garnier@irisa.fr',
     url='https://github.com/askomics/askocli',
-    download_url='https://github.com/askomics/askocli/archive/0.4.1.tar.gz',
+    download_url='https://github.com/askomics/askocli/archive/0.4.2.tar.gz',
     install_requires=['requests>=2.4.3'],
     packages=find_packages(),
     license='AGPL',
     platforms='Posix; MacOS X; Windows',
     scripts=['bin/' + f for f in listdir('bin')],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
@@ -27,5 +27,6 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ])
